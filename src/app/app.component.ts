80c5465f9b15efd50e85c9ecc972e1c97e6b780e
Artem <lobach.artem.v@gmail.com>
2021-12-09 19:01:10 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'app',
+  template: '<router-outlet></router-outlet>'
+})
+export class AppComponent {
+}
